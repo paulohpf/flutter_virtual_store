05f70eea29f83241ffba371a36d5d234b0ef5fb4
Paulo Henrique Palmeira Franco <paulohpfranco@uol.com.br>
2021-05-03 16:29:56 -0300

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_virtual_store/screens/home_screen.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -14,7 +19,19 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: FutureBuilder<FirebaseApp>(
+        future: _initialization,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return HomeScreen();
+          }
+
+          return const CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+          );
+        },
+      ),
     );
   }
 }
--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,10 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
     Widget _buildBodyBack() => Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
                 Color.fromARGB(255, 211, 118, 130),
@@ -21,7 +26,7 @@ class HomeTab extends StatelessWidget {
         _buildBodyBack(),
         CustomScrollView(
           slivers: <Widget>[
-            SliverAppBar(
+            const SliverAppBar(
               floating: true,
               snap: true,
               backgroundColor: Colors.transparent,
@@ -31,6 +36,41 @@ class HomeTab extends StatelessWidget {
                 centerTitle: true,
               ),
             ),
+            FutureBuilder<QuerySnapshot>(
+              future: firestore.collection('home').orderBy('pos').get(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                }
+
+                return SliverStaggeredGrid.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 1,
+                  crossAxisSpacing: 1,
+                  staggeredTiles:
+                      snapshot.data.docs.map((QueryDocumentSnapshot doc) {
+                    return StaggeredTile.count(doc.data()['x'] as int,
+                        double.parse(doc.data()['y'].toString()));
+                  }).toList(),
+                  children: snapshot.data.docs.map((QueryDocumentSnapshot doc) {
+                    return FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: doc.data()['image'] as String,
+                      fit: BoxFit.cover,
+                    );
+                  }).toList(),
+                );
+              },
+            )
           ],
         ),
       ],
